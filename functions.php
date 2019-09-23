4bb6bf98fdfcec88d25f4f487dc5832cf2fd1c0a
Tom Brooks <tom@olospo.co.uk>
2019-09-23 15:54:44 +0100

    Services CPT

--- functions.php
+++ functions.php
@@ -75,3 +75,61 @@ function km_get_user_role( $user = null ) {
 	$user = $user ? new WP_User( $user ) : wp_get_current_user();
 	return $user->roles ? $user->roles[0] : false;
 }
+
+function custom_post_type() {
+  
+  // Services Post Type
+	$labels = array(
+		'name'                => _x( 'Services', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Service', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Services', 'text_domain' ),
+		'all_items'           => __( 'All Services', 'text_domain' ),
+		'view_item'           => __( 'View Service', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Service', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Service', 'text_domain' ),
+		'update_item'         => __( 'Update Service', 'text_domain' ),
+		'search_items'        => __( 'Search Services', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$args = array(
+		'label'               => __( 'Services', 'text_domain' ),
+		'description'         => __( 'Moorland Fuels Services', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom fields' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 20,
+		'menu_icon'           => 'dashicons-admin-tools',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'services', $args );
+
+		
+}
+// Hook into the 'init' action
+add_action( 'init', 'custom_post_type', 0 );
+
+add_action( 'init', 'create_service_tax' );
+
+function create_service_tax() {
+	register_taxonomy(
+		'type',
+		'services',
+		array(
+			'label' => __( 'Service Type' ),
+			'rewrite' => array( 'slug' => 'type' ),
+			'hierarchical' => true,
+			'show_admin_column' => true,
+		)
+	);
+}
